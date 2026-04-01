5e7fc8549b7fc92f480548d32e215c9c47662100
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-01 16:55:23 -0300

    Condições parte 1

---
+++ exercicios/aula11/ex01.js
@@ -0,0 +1,13 @@
+var vel = 150
+console.log(`A velocidade do seu carro é ${vel}Km/h`)
+console.log(`Dirija sempre usando cinto de segurança!`)
+
+if(vel > 70) {
+    console.log(`Você ultrapassou a velocidade permitida. Está multado!`)
+} else {
+    console.log(`Você está dirigindo corretamente!`)
+}
+
+if (vel => 150) {
+    console.log(`Se continuar, Deus te aguarda!`)
+}
---
+++ exercicios/aula11/ex02.js
@@ -0,0 +1,9 @@
+var pais = 'brasil'
+
+var res = pais.toUpperCase()
+
+if (res == 'BRASIL') {
+    console.log(`Você é brasileiro!`)
+} else {
+    console.log(`Você é estrangeiro!`)
+}
