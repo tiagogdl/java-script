cbec3571b8fb9a1f62d21244f59bf614754c9680
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-02 22:54:43 -0300

    Exercício Verificador de Idade

---
+++ exercicios/aula12-ex/ex02/script.js
@@ -0,0 +1,59 @@
+function verificar() {
+    
+    var ano = document.getElementById('ano')
+    var res = document.getElementById('res')
+
+    var atual = new Date()
+    var anoatual = atual.getFullYear()
+
+    if (ano.value.length == 0 || Number(ano.value) > anoatual) {
+        window.alert('Verifique os dados e tente novamente!')
+    } else {
+        var sexo = document.getElementsByName('sexo')
+        var idade = anoatual - Number(ano.value)
+        var genero = ''
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (sexo[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.src = 'imagens/imagens-redondas/criança-masc.png'
+            } else if (idade >= 10 && idade < 18) {
+                //adolescente
+                img.src = 'imagens/imagens-redondas/adolescente-masc.png'
+            } else if (idade >= 18 && idade < 30) {
+                //adulto
+                img.src = 'imagens/imagens-redondas/adulto-masc.png'
+            } else if (idade >= 30 && idade< 60) {
+                //velho
+                img.src = 'imagens/imagens-redondas/velho-masc.png'
+            } else {
+                //idoso
+                img.src = 'imagens/imagens-redondas/idoso-masc.png'
+            }
+
+        } else {
+            genero = 'Mulher'
+
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.src = 'imagens/imagens-redondas/criança-fem.png'
+            } else if (idade >= 10 && idade < 18) {
+                //adolescente
+                img.src = 'imagens/imagens-redondas/adolescente-fem.png'
+            } else if (idade >= 18 && idade < 30) {
+                //adulto
+                img.src = 'imagens/imagens-redondas/adulto-fem.png'
+            } else if (idade >= 30 && idade< 60) {
+                //velho
+                img.src = 'imagens/imagens-redondas/velho-fem.png'
+            } else {
+                //idoso
+                img.src = 'imagens/imagens-redondas/idoso-fem.png'
+            }
+        }
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
+    }
+}
