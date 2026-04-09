7bda2604df7a7a0825ed819628906a064123e1ad
Tiago Dias <tiagodiasgdl474@gmail.com>
2026-04-09 16:23:13 -0300

    Feito condição

--- exercicios/aula14-ex/ex02/script.js
+++ exercicios/aula14-ex/ex02/script.js
@@ -1,13 +1,17 @@
 function gerar() {
     var num_text = document.querySelector('input#num')
-    var num = Number(num_text.value)
     var res = document.querySelector('select#res')
     res.innerHTML= ''
 
-    for (c = 1; c <= 10; c = c + 1) {
-        var tab = num * c
-        var item = document.createElement('option')
-        item.text = `${num} x ${c} = ${tab}`
-        res.appendChild(item)
+    if (num_text.value.length == 0) {
+        window.alert('Por favor digite um número!')
+    } else {
+        var num = Number(num_text.value)
+        for (c = 1; c <= 10; c = c + 1) {
+            var tab = num * c
+            var item = document.createElement('option')
+            item.text = `${num} x ${c} = ${tab}`
+            res.appendChild(item)
+        }
     }
-}
+}   
